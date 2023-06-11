7dc061c39897c2e5bd8d44caabc4fcd96b3658c2
Arnav <arnav.jose@infoworks.io>
2023-06-11 22:01:56 +0530

    flake works

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-	description = "Installing packages for kubernetes development on MacOS"
+	description = "Installing packages for kubernetes development on MacOS";
 
 	inputs = {
 	  nixpkgs = {
@@ -8,14 +8,15 @@
   };
 
   outputs = {self, nixpkgs}: {
-    packages.x86-64.darwin.bash = nixpkgs.legacyPackages.x86_64-darwin.bash;
-    packages.x86-64.darwin.gnused = nixpkgs.legacyPackages.x86-64.darwin.gnused;
-    packages.x86-64.darwin.podman = nixpkgs.legacyPackages.x86-64.darwin.podman;
-    packages.x86-64.darwin.openjdk8-bootstrap = nixpkgs.legacyPackages.x86-64.darwin.openjdk8-bootstrap;
-    packages.x86-64.darwin.kubectl = nixpkgs.legacyPackages.x86-64.darwin.kubectl;
-    packages.x86-64.darwin.skaffold = nixpkgs.legacyPackages.x86-64.darwin.skaffold;
-    packages.x86-64.darwin.kind = nixpkgs.legacyPackages.x86-64.darwin.kind;
-    packages.x86-64.darwin.kubernetes-helm = nixpkgs.legacyPackages.x86-64.darwin.kubernetes-helm;
-  }
+    packages.x86_64-darwin.bash = nixpkgs.legacyPackages.x86_64-darwin.bash;
+    packages.x86_64-darwin.gnused = nixpkgs.legacyPackages.x86_64-darwin.gnused;
+    packages.x86_64-darwin.podman = nixpkgs.legacyPackages.x86_64-darwin.podman;
+    packages.x86_64-darwin.openjdk8-bootstrap = nixpkgs.legacyPackages.x86_64-darwin.openjdk8-bootstrap;
+    packages.x86_64-darwin.kubectl = nixpkgs.legacyPackages.x86_64-darwin.kubectl;
+    packages.x86_64-darwin.skaffold = nixpkgs.legacyPackages.x86_64-darwin.skaffold;
+    packages.x86_64-darwin.kind = nixpkgs.legacyPackages.x86_64-darwin.kind;
+    packages.x86_64-darwin.kubernetes-helm = nixpkgs.legacyPackages.x86_64-darwin.kubernetes-helm;
+    packages.x86_64-darwin.default = self.packages.x86_64-darwin.bash;
+  };
 
 }
